0202f0b54aba8d2c8e644ff58a67200e4fb262e9
Nicolas Constanty <nicolas.constanty@epitech.eu>
2016-03-09 18:29:14 +0100

    open dl

--- inc/Arcade.hpp
+++ inc/Arcade.hpp
@@ -5,25 +5,33 @@
 ** Login	consta_n
 **
 ** Started on	Tue Mar 08 18:23:04 2016 Nicolas Constanty
-** Last update	Wed Mar 09 16:53:01 2016 Nicolas Constanty
+** Last update	Wed Mar 09 18:25:07 2016 Nicolas Constanty
 */
 
 #ifndef ARCADE_HPP_
 # define ARCADE_HPP_
 
 # include <dlfcn.h>
+# include <vector>
+#include <dirent.h>
+#include <stdio.h>
+#include <algorithm>
 # include "Utils.hpp"
 # include "Interfaces.hpp"
 
 enum SOTYPE { GRAPH, GAME };
 
 class Arcade {
+private:
+  std::vector<std::string> graph_paths;
+  std::vector<std::string> game_paths;
 public:
   Arcade ();
   virtual ~Arcade ();
-  static void* initSo(std::string const &, SOTYPE);
-  static IGame*  initGame(void *ptr);
-  static IGraph* initGraph(void *ptr);
+  void* initSo(std::string const &, SOTYPE);
+  IGame*  initGame(void *ptr);
+  IGraph* initGraph(void *ptr);
+  void		refresh_lib(std::string const &folder);
 };
 
 #endif /* !ARCADE_HPP_ */
--- src/Arcade.cpp
+++ src/Arcade.cpp
@@ -5,11 +5,38 @@
 ** Login	consta_n
 **
 ** Started on	Tue Mar 08 18:28:59 2016 Nicolas Constanty
-** Last update	Wed Mar 09 16:54:51 2016 Nicolas Constanty
+** Last update	Wed Mar 09 18:26:59 2016 Nicolas Constanty
 */
 
 #include <iostream>
 #include "Arcade.hpp"
+
+Arcade::Arcade()
+{
+  this->refresh_lib("./lib/");
+  this->refresh_lib("./games/");
+}
+
+Arcade::~Arcade()
+{}
+
+void Arcade::refresh_lib(std::string const &folder)
+{
+  DIR *dir;
+  struct dirent *ent;
+  if ((dir = opendir (folder.c_str())) != NULL) {
+    while ((ent = readdir (dir)) != NULL) {
+      if (std::string(ent->d_name).find(".so") != std::string::npos)
+      {
+        std::cout << ent->d_name << std::endl;
+        if (std::find(this->game_paths.begin(), this->game_paths.end(), ent->d_name) != this->game_paths.end())
+          this->game_paths.push_back(ent->d_name);
+      }
+    }
+  } else {
+    perror ("");
+  }
+}
 
 IGraph *Arcade::initGraph(void *ptr)
 {
@@ -19,13 +46,9 @@ IGraph *Arcade::initGraph(void *ptr)
   fptr load_lib = (IGraph *(*)())(dlsym(ptr, lib_name));
   const char *dlsym_error = dlerror();
   if (dlsym_error)
-  {
     std::cerr << "Cannot load symbol '" << lib_name << "': " << dlsym_error << '\n';
-  }
   else
-  {
     return ((IGraph *)load_lib());
-  }
   std::cout << "EndInit => Graph" << std::endl;
   return (NULL);
 }
@@ -38,13 +61,9 @@ IGame *Arcade::initGame(void *ptr)
   fptr load_game = (IGame *(*)())(dlsym(ptr, lib_name));
   const char *dlsym_error = dlerror();
   if (dlsym_error)
-  {
     std::cerr << "Cannot load symbol '" << lib_name << "': " << dlsym_error << '\n';
-  }
   else
-  {
     return ((IGame *)load_game());
-  }
   std::cout << "EndInit => Game" << std::endl;
   return (NULL);
 }
@@ -53,9 +72,15 @@ void	*Arcade::initSo(std::string const &name, SOTYPE type)
 {
   //reset error
   dlerror();
-  void *myso = dlopen(std::string("./" +
+  void *myso;
+  if (name.find(".so") == std::string::npos)
+  {
+    myso = dlopen(std::string("./" +
     ((type == GAME)?std::string("games"):std::string("lib")) +
     "/lib_arcade_" + name + ".so").c_str(), RTLD_LAZY);
+  }
+  else
+    myso = dlopen(name.c_str(), RTLD_LAZY);
   if (!myso)
   {
     std::cerr << "Cannot open library: " << dlerror() << '\n';
@@ -68,11 +93,17 @@ void	*Arcade::initSo(std::string const &name, SOTYPE type)
   return (NULL);
 }
 
-int	main()
+int	main(int ac, char **av)
 {
-
-  IGraph *graph = (IGraph *)Arcade::initSo("ncurses", GRAPH);
-  IGame *game = (IGame *)Arcade::initSo("snake", GAME);
+  //Make usage
+  if (ac < 2)
+  {
+    std::cout << "USAGE : ./arcade [LIB_PATH.so]" << std::endl;
+    return (1);
+  }
+  Arcade *arcade = new Arcade();
+  IGraph *graph = (IGraph *)arcade->initSo(av[1], GRAPH);
+  IGame *game = (IGame *)arcade->initSo("snake", GAME);
   (void)game;
   (void)graph;
   return (0);
